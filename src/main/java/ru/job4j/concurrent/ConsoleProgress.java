838c1b595d520200db7f7981789e258fb3823214
krutaxe <krutaxe@mail.ru>
2022-04-30 17:59:16 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        String[] process = new String[] {"--", "\\", "|", "/" };
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                for (String s : process) {
+                    Thread.sleep(250);
+                    System.out.print("\r load: " + s);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+}
