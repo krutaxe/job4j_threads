08436198caeeb2e72576bc92e89d75fa415b90a3
krutaxe <krutaxe@mail.ru>
2022-04-30 15:54:28 +0300

    1.1. Состояние нити. [#229175 #295490]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> { }
+        );
+
+        Thread second = new Thread(
+                () -> { }
+        );
+
+        first.start();
+        second.start();
+
+        while (first.getState() == Thread.State.RUNNABLE
+                || second.getState() == Thread.State.RUNNABLE) {
+            System.out.println(first.getName());
+            System.out.println(second.getName());
+        }
+        System.out.println("Работа нитей first и second завершина");
+    }
+}
