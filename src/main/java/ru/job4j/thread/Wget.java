dad3034f884f309c58a3a8cd027d8d2404b49f0c
krutaxe <krutaxe@mail.ru>
2022-05-07 00:27:03 +0300

    4. Скачивание файла с ограничением. [#144271 #296165]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -8,38 +8,42 @@ public class Wget implements Runnable {
     private final String url;
     private final int speed;
 
+    private final String fileName;
+
     public static long downloadSpeed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("10Mb_tmp.dat")) {
+             FileOutputStream out = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesReads;
-            int bufferDownload = 0;
-            long time;
+            int bytesWrited = 0;
+            long deltaTime;
             long start = System.currentTimeMillis();
             while ((in.read()) != -1) {
                 bytesReads = in.read(dataBuffer, 0, 1024);
                 out.write(dataBuffer, 0, bytesReads);
-                bufferDownload += bytesReads;
-            }
-            long end = System.currentTimeMillis();
-            time = end - start;
-            downloadSpeed = (bufferDownload) / (time / 1000);
-            if ((downloadSpeed) > (speed)) {
-                long timeSleep = ((bufferDownload / speed) * 1000L) - time;
-                if (timeSleep > 0) {
-                    try {
-                        Thread.sleep(timeSleep);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                bytesWrited += bytesReads;
+                if (bytesWrited >= speed) {
+                    long end = System.currentTimeMillis();
+                    deltaTime = (end - start);
+                    if (deltaTime < 1000) {
+                        System.out.println(deltaTime);
+                        try {
+                            Thread.sleep(1000 - deltaTime);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
+                    start = System.currentTimeMillis();
+                    bytesWrited = 0;
                 }
             }
         } catch (IOException e) {
@@ -49,16 +53,20 @@ public class Wget implements Runnable {
     }
 
     public static void validate(String[] args) {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Incorrect parameters");
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Incorrect parameters!!!"
+                    + "\n 1 parameter: url (http://...)"
+                    + "\n 2 parameter: limit speed downloads (b/s)"
+                    + "\n 3 parameter: name new file (text)");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         validate(args);
         String url = args[0];
+        String fileName = args[2];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
