72be61de89372f1978d138fcd3e8acb8917822a5
krutaxe <krutaxe@mail.ru>
2022-05-06 23:36:12 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -0,0 +1,94 @@
+package ru.job4j;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            monitor.notifyAll();
+            count++;
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count >= total) {
+                count();
+                return;
+            }
+            try {
+                monitor.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountBarrier countBarrier = new CountBarrier(1);
+        Thread first = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName()
+                            + " " + Thread.currentThread().getState());
+                },
+                "first");
+
+        Thread second = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName()
+                            + " " + Thread.currentThread().getState());
+                },
+                "second");
+
+        Thread three = new Thread(
+                () -> {
+                    countBarrier.count();
+                    System.out.println(Thread.currentThread().getName()
+                            + " " + Thread.currentThread().getState());
+                },
+                "three");
+
+        Thread four = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName()
+                            + " " + Thread.currentThread().getState());
+                },
+                "four");
+
+        first.start();
+        second.start();
+
+        Thread.sleep(500);
+
+
+        System.out.println("поток 1 - " + first.getState());
+        System.out.println("поток 2 - " + second.getState());
+        System.out.println("поток 3 - " + three.getState());
+
+        System.out.println("_____________");
+
+        three.start();
+
+        four.start();
+
+        Thread.sleep(2000);
+
+        System.out.println("_____________");
+
+        System.out.println("поток 1 - " + first.getState());
+        System.out.println("поток 2 - " + second.getState());
+        System.out.println("поток 3 - " + three.getState());
+        System.out.println("поток 4 - " + four.getState());
+    }
+}
