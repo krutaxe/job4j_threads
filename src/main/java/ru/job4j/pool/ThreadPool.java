190ef33c56d2349b37f6d6d960c8b28528e2199b
krutaxe <krutaxe@mail.ru>
2022-05-14 23:41:04 +0300

    1. Реализовать ThreadPool [#1099 #300748]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -4,30 +4,58 @@ import ru.job4j.SimpleBlockingQueue;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ThreadPool {
     private final int size = Runtime.getRuntime().availableProcessors();
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
-    public void work(Runnable job) {
-        try {
-            tasks.offer(job);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    public ThreadPool(SimpleBlockingQueue<Runnable> tasks) throws InterruptedException {
+        for (int i = 1; i <= size; i++) {
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (!tasks.isEmpty()) {
+                            work(tasks.poll());
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }));
+            threads.get(i - 1).start();
+        }
+        shutdown();
+    }
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() throws InterruptedException {
+        for (Thread thread : threads) {
+            thread.interrupt();
+            thread.join();
         }
     }
 
-    public void shutdown() {
-        for (Thread thread: threads) {
-            thread.interrupt();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+    public static void main(String[] args) throws InterruptedException {
+        int size = Runtime.getRuntime().availableProcessors();
+        SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
+
+        for (int i = 1; i <= size; i++) {
+            tasks.offer(new Runnable() {
+                @Override
+                public void run() {
+                    int rsl = 0;
+                    for (int i = 0; i < 1000000; i++) {
+                        rsl += i;
+                    }
+                }
+            });
         }
+
+        new ThreadPool(tasks);
     }
 }
