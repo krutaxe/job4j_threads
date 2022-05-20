2aecb4d467a815b84bdbfdc2757f34ff8994efb5
krutaxe <krutaxe@mail.ru>
2022-05-20 13:42:02 +0300

    1

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -95,18 +95,4 @@ public class RolColSum {
             return new Sums(rowSum, colSum);
         });
     }
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int[][] array5 = {{1, 2, 3, 4, 7},
-                {5, 6, 7, 8, 8},
-                {9, 10, 11, 12, 9},
-                {13, 14, 15, 16, 0},
-                {3, 4, 5, 6, 7}};
-
-        int[][] array3 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-        System.out.println(getTask(array3, 0).get());
-        System.out.println(Arrays.toString(asyncSum(array5)));
-        System.out.println(Arrays.toString(sum(array5)));
-    }
 }
